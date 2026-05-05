a96846c26f54b4558ba2851acb71c34df35b4f8b
GabrielleZeti <69438737+GabrielleZeti@users.noreply.github.com>
2026-05-05 15:06:27 -0600

    Labo03

--- Labo03/app/src/main/java/com/example/labo03/Screens.kt
+++ Labo03/app/src/main/java/com/example/labo03/Screens.kt
@@ -1,5 +1,7 @@
 package com.example.labo03
 
+//pantalla
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
